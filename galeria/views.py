5188deae29d667239340c3395b7b1623ad65ddf5
Murilo Fischer <mufischer@hotmail.com>
2024-04-30 09:59:20 -0300

    arquivos estáticos

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'alura-space/index.html')
+
+def imagem(request):
+    return render (request, 'alura-space/imagem.html')
